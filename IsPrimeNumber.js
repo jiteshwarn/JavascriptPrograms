fc47373eaa1c402603e23d9599a9d854535e766c
Bhupendra Singh Parihar <bhupendraparihar@outlook.com>
2016-09-14 23:44:44 +0530

    Create IsPrimeNumber.js

---
+++ IsPrimeNumber.js
@@ -0,0 +1,42 @@
+//School Way
+function isPrime(n){
+  //Corner case
+  if(n <= 1) return false; 
+
+  for(var i = 2;i < n; i++){
+    if(n%i===0){
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+//Optimized Way
+function isPrime(n){
+  //Corner case
+  if(n <= 1) return false;
+  
+  for(var i = 2; i*i <= n; i++){
+    if(n%i===0){
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+//6K+-1 form : All the prime numbers are of this form
+function isPrime(n){
+  //Corner case
+  if(n <= 1) return false;
+  
+  if(n <= 3) return true;
+  
+  for(var i = 5; i*i <=n; i=i+6){
+    if(n%i === 0 || n%(i+2) === 0){
+      return false;
+    }
+  }
+  return true;
+}
